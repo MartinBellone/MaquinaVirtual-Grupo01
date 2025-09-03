3dabec70a96216a9f62ff09aaf6d80a450416c83
Enzo Sorrenti <Enzosorrenti5@gmail.com>
2025-09-02 21:59:47 -0300

    Correccion de sintaxis en VirtualMachine.c

--- VirtualMachine.c
+++ VirtualMachine.c
@@ -28,14 +28,14 @@ void writeMemory(){
 
 }
 
-void readFile(TVM *vm,char[] name){     //funcion para leer el vmx
+void readFile(TVM *vm, char name[]){     //funcion para leer el vmx
     FILE *arch;
     int i=0; //direccion de memoria a guardar el byte
     char c;
 
     arch=fopen(name,"rb");
     if (arch==NULL)
-        printf("ERROR al abrir el archivo")
+        printf("ERROR al abrir el archivo");
     else
         while (fread(&c,sizeof(char),1,arch)==1){   //TODO: revisar fread
             vm->mem[i]=c;
@@ -43,13 +43,14 @@ void readFile(TVM *vm,char[] name){     //funcion para leer el vmx
         }
         initVm(vm,i); 
 }
+
 void readInstruction(TVM *vm){
     char instruction;
     int maskOPC=0b00011111;  //mascara para obtener el codigo de operacion
     int maskTOP1=0b00110000;  //mascara para obtener el primer operando
     int maskTOP2=0b11000000;  //mascara para obtener el segundo operando
     int TOP1,TOP2;
-    intruction= vm->mem[vm->reg[IP]]; 
+    instruction= vm->mem[vm->reg[IP]];
 
     TOP2= (instruction & maskTOP2)>>6;
     TOP1= (instruction & maskTOP1)>>4;
