b21ee197aeeb23ee4c8e4439f97f6311cce150b7
unknown <m.bellone@yahoo.com>
2025-10-16 15:20:06 -0300

    adding Sys7 y Sys8

--- include/VirtualMachine.h
+++ include/VirtualMachine.h
@@ -19,9 +19,11 @@ typedef struct {
     unsigned char *mem;  // char mem[16]
     int reg[32];         // 4 Bytes por cada entero, son 32 registros de 4 bytes cada uno
     TSR tableSeg[8];
+    char *vmiFile;
     int argc;
     char **argv;
-} TVM;                                                                                // Type virtual machine
+} TVM;                   
+                                                             // Type virtual machine
 void initTSR(TVM *vm, unsigned short int sizes[7], unsigned short int cantSegments);  // Funcion para inicializar la tabla de segmentos
 void readFileVMX(TVM *vm, char *fileName);                                            // Funcion para leer el archivo vmx
 void initVm(TVM *vm, unsigned short int sizes[7], unsigned short int cantSegments);   // Funcion para inicializar la maquina virtual
@@ -30,8 +32,9 @@ void readOp(TVM *vm, int TOP, int numOp);
 void showCodeSegment(TVM *vm);
 void executeProgram(TVM *vm);
 void executeDisassembly(TVM *vm);
-void parseArgs(int argc, char *argv[], VMParams *args);
+void parseArgs(int argc, char *argv[], VMParams *args, TVM *vm);
 void buildParamSegment(TVM *vm, VMParams *params);
-void MOV(TVM *vm, int tipoOp1, int tipoOp2);
+void readFileVMI(TVM *vm, char *fileName);
+void writeFile(TVM *vm, char *fileName);
 
 #endif  // VIRTUAL_MACHINE_H
